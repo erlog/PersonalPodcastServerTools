be3cac6b7a74342af0925c39e20de27a66874e92
John Rozewicki <jjrozewicki@gmail.com>
2016-02-02 01:51:49 -0500

    forgot to fix this after I did code clean-up on podcastgeneratorlib.rb

--- podcastindex.rb
+++ podcastindex.rb
@@ -4,7 +4,7 @@ require_relative 'podcastgeneratorlib'
 #For if you want a username/password inlined into the podcast URL's
 NetRCFilePath = "/etc/apache2/.netrc"
 
-def getfulluri()
+def get_full_uri()
 	uri = URI("http://blank.blank")
 	uri.host = ENV["SERVER_NAME"]
 	uri.path = ENV["REQUEST_URI"]
@@ -12,13 +12,15 @@ def getfulluri()
 	return uri
 end
 
-rssuri = getfulluri() 
-title = rssuri.path
-description = "Index of %s on %s" % [rssuri.path, rssuri.host]
-podcast = Podcast.new(rssuri, title, description)
-localpath = File.join(ENV["DOCUMENT_ROOT"], ENV["REQUEST_URI"])
+rss_uri = get_full_uri()
+title = rss_uri.path
+description = "Index of %s on %s" % [rss_uri.path, rss_uri.host]
+local_path = File.join(ENV["DOCUMENT_ROOT"], ENV["REQUEST_URI"])
+
+podcast = Podcast.new(rss_uri, title, description)
+podcast.items = index_local_directory(local_path, rss_uri, NetRCFilePath)
+podcast.items.sort_by!(&:pubdate).reverse!
 
 puts "Content-type: application/xml\n\n"
-podcast.items = indexlocaldirectory(localpath, rssuri, NetRCFilePath)
-puts podcast.getXML
+puts podcast.get_xml
 
