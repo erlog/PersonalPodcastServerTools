973efc9ead8e16bed4b4d3f464acae3ec5bed563
John Rozewicki <jjrozewicki@gmail.com>
2016-02-02 00:40:01 -0500

    code cleanup and added support for more arguments in the media list file

--- podcastgeneratorlib.rb
+++ podcastgeneratorlib.rb
@@ -19,34 +19,34 @@ MIMETypeCommand = "file --mime-type "
 class Podcast
 	def initialize(rssurl, title, description)
 		@items = []
-		@rssurl = rssurl
+		@rss_url = rssurl
 		@title = title
 		@description = description
 	end
 	
 	attr_accessor :items
 
-	def generateheader()
+	def generate_header()
 		header = ['<?xml version="1.0" encoding="UTF-8"?>']
 		header << '<rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">'
 		header << '<channel>'
-		header << "<atom:link href=\"%s\" rel=\"self\" type=\"application/rss+xml\"/>" % @rssurl 
-		header << bracketizeXML("link", @rssurl)
-		header << bracketizeXML("title", @title)
-		header << bracketizeXML("description", @description)
+		header << "<atom:link href=\"%s\" rel=\"self\" type=\"application/rss+xml\"/>" % @rss_url 
+		header << bracketize_xml("link", @rss_url)
+		header << bracketize_xml("title", @title)
+		header << bracketize_xml("description", @description)
 		return header.join("\n")
 	end
 
-	def getXML()
+	def get_xml()
 		itemsxml = [] 
 
 		@items.each do |item|
-			itemsxml << item.getXML
+			itemsxml << item.get_xml
 		end
-		return generateheader + itemsxml.join("\n\n") + generatefooter
+		return generate_header + itemsxml.join("\n\n") + generate_footer
 	end
 
-	def generatefooter()
+	def generate_footer()
 		return ['</channel>', '</rss>'].join("\n")
 	end
 end
@@ -73,23 +73,23 @@ class PodcastItem
 		other.pubdate <=> @pubdate
 	end
 	
-	def getXML()
+	def get_xml()
 		lines = ['<item>']
-		lines << bracketizeXML('title', CGI.escapeHTML(@title)) 
-		lines << bracketizeXML('link', @url)
+		lines << bracketize_xml('title', CGI.escapeHTML(@title)) 
+		lines << bracketize_xml('link', @url)
 		lines << "<guid isPermaLink=\"false\">%s</guid>" % @url
-		lines << bracketizeXML('pubDate', @pubdate.httpdate)
-		lines << "<enclosure%s%s%s/>" % [ parameterizeXML(" url", @url), 
-								parameterizeXML(" type", @mimetype),
-								parameterizeXML(" length", @filesize) ] 
+		lines << bracketize_xml('pubDate', @pubdate.httpdate)
+		lines << "<enclosure%s%s%s/>" % [ parameterize_xml(" url", @url), 
+								parameterize_xml(" type", @mimetype),
+								parameterize_xml(" length", @filesize) ] 
 		lines << '</item>'
 		return lines.join("\n") 
 	end
 
-	def constructitemforfileURI(uri)
-		cached = loadfromcache(uri.to_s)
+	def construct_item_for_file_uri(uri)
+		cached = load_from_cache(uri.to_s)
 		if cached
-			parseditem = constructitemfromXML(cached) 
+			parseditem = construct_item_from_xml(cached) 
 			return parseditem unless !parseditem
 		end	
 
@@ -101,8 +101,8 @@ class PodcastItem
 		
 		request = Net::HTTP::Head.new(uri.request_uri)
 		if uri.userinfo
-			request.basic_auth(unescapeXMLURL(uri.user), 
-						unescapeXMLURL(uri.password)) 
+			request.basic_auth(unescape_xml_url(uri.user), 
+						unescape_xml_url(uri.password)) 
 		end
 
 		response = http.request(request)
@@ -118,11 +118,11 @@ class PodcastItem
 			@pubdate = DateTime.now
 		end
 		
-		savetocache(uri.to_s, getXML)
+		save_to_cache(uri.to_s, get_xml)
 		return self
 	end
 
-	def constructitemforfile(localfilepath, uri)
+	def construct_item_for_file(localfilepath, uri)
 		filename = File.basename(localfilepath) 
 		escapedpath = Shellwords.escape(localfilepath)
 
@@ -134,7 +134,7 @@ class PodcastItem
 		return self
 	end
 
-	def constructitemfromXML(xmlstring)
+	def construct_item_from_xml(xmlstring)
 		item = REXML::XPath.match(REXML::Document.new(xmlstring), "//item")[0]
 		return item if !item
 		@title = item.elements["title"].text
@@ -146,59 +146,60 @@ class PodcastItem
 	end
 end
 
-def syncYouTubePlaylist(url, downloadfolderpath)
+def sync_youtube_playlist(url, downloadfolderpath, formatcode)
 	downloadfolderpath += "/" unless downloadfolderpath[-1] == "/"
-	command = "youtube-dl "\
-			"--max-downloads 10 "\
-			"--playlist-end 10 "\
-			"--youtube-skip-dash-manifest "\
-			"--date today "\
-			"-o \"#{downloadfolderpath}%(title)s-%(id)s.%(ext)s\" "\
-			"\"#{url}\""
+	command = "youtube-dl "
+	command += "--max-downloads 10 "
+	command += "--playlist-end 10 "
+	command += "--youtube-skip-dash-manifest "
+	command += "-f #{formatcode} " if formatcode
+	command += "--dateafter today-2days "
+	command += "-o \"#{downloadfolderpath}%(title)s-%(id)s.%(ext)s\" "
+	command += "\"#{url}\""
 	return system(command)
 end
 
-def downloadYouTubeVideo(url, downloadfolderpath, formatcode)
+def download_youtube_video(url, downloadfolderpath, formatcode)
+	downloadfolderpath += "/" unless downloadfolderpath[-1] == "/"
 	command = "youtube-dl "
 	command += "--youtube-skip-dash-manifest "
 	command += "-f #{formatcode} " if formatcode
-	downloadfolderpath += "/" unless downloadfolderpath[-1] == "/"
 	command += "-o #{downloadfolderpath}\"%(title)s-%(id)s.%(ext)s\" "
 	command += "\"#{url}\""
 	return system(command)
 end
 
-def bracketizeXML(tagname, content)
+def bracketize_xml(tagname, content)
 	return "<%s>%s</%s>" % [tagname, content, tagname]
 end
 
-def escapeXMLURL(url)
+def escape_xml_url(url)
 	url = URI.escape(url.to_s)
 	url = url.gsub("&", "%26").gsub("'", "%27")
 	return url
 end
 
-def unescapeXMLURL(url)
+def unescape_xml_url(url)
 	url = URI.unescape(url)
 	url = url.gsub("%26", "&").gsub("%27", "'")
 	return url
 end
 
-def parseURL(url, netrcfile = nil)
-	uri = URI(escapeXMLURL(url))
+def parse_url(url, netrcfile = nil)
+	uri = URI(escape_xml_url(url))
 	if netrcfile
 		credentials = Netrc.read(netrcfile)[uri.host]
-		uri.user = escapeXMLURL(credentials[0])
-		uri.password = escapeXMLURL(credentials[1])
+		uri.user = escape_xml_url(credentials[0])
+		uri.password = escape_xml_url(credentials[1])
 	end
 	return uri
 end
 
-def parameterizeXML(paramatername, string)
+def parameterize_xml(paramatername, string)
 	return "%s=\"%s\"" % [paramatername, string]
 end
 
-def indexlocaldirectory(localpath, httpfolderurl, netrcfile = nil)
+def index_local_directory(localpath, httpfolderurl, netrcfile = nil)
 	filepaths = []
 	Dir::entries(localpath).each do |entry|
 		filepath = File.join(localpath, entry)
@@ -207,16 +208,16 @@ def indexlocaldirectory(localpath, httpfolderurl, netrcfile = nil)
 		end
 	end
 
-	uris = buildURIsforfiles(filepaths, httpfolderurl, netrcfile)
+	uris = build_uris_for_files(filepaths, httpfolderurl, netrcfile)
 
 	items = []
 	filepaths.zip(uris).each do |path, uri|
-		items << PodcastItem.new.constructitemforfile(path, uri)
+		items << PodcastItem.new.construct_item_for_file(path, uri)
 	end
 	return items	
 end
 
-def indexremotedirectory(hostname, remotepath, httpfolderurl, netrcfile = nil)
+def index_remote_directory(hostname, remotepath, httpfolderurl, netrcfile = nil)
 	username, password = Netrc.read(netrcfile)[hostname]
 	filepaths = []
 	Net::SFTP.start(hostname, username, :password => password) do |sftp|
@@ -225,33 +226,37 @@ def indexremotedirectory(hostname, remotepath, httpfolderurl, netrcfile = nil)
 		end
 	end
 
-	uris = buildURIsforfiles(filepaths, httpfolderurl, netrcfile)
+	uris = build_uris_for_files(filepaths, httpfolderurl, netrcfile)
 
 	items = []
 	uris.each do |uri|
-		items << PodcastItem.new.constructitemforfileURI(uri)
+		items << PodcastItem.new.construct_item_for_file_uri(uri)
 	end
 
 	return items	
 end
 
-def buildURIsforfiles(filepaths, httpfolderurl, netrcfile = nil)
-	folderURI = parseURL(httpfolderurl, netrcfile)
+def build_uris_for_files(filepaths, httpfolderurl, netrcfile = nil)
+	folderURI = parse_url(httpfolderurl, netrcfile)
 	uris	= []
 	filepaths.each do |filepath|
-		escaped = escapeXMLURL(File.basename(filepath))
+		escaped = escape_xml_url(File.basename(filepath))
 		uris << URI.join(folderURI, escaped)
 	end
 	return uris 
 end
 
-def parsemedialist()
+def parse_media_list(media_list_path)
 	items = []
-	lines = open(MediaListPath).read.split("\n").map(&:strip)
+	lines = open(media_list_path).read.split("\n").map(&:strip)
 	lines.each do |line|
-		type, argument = line.split("||", 2)
-		items << [type.downcase, argument]	
+		split = line.split("||")
+		type = split.slice!(0) 
+		path = split.slice!(-1) 
+		arguments = split 
+		items << [type.downcase, arguments, path]	
 	end
+	print items
 	return items
 end
 
@@ -259,13 +264,13 @@ def md5(string)
 	return Digest::MD5.new.update(string).hexdigest
 end
 
-def savetocache(string, data)
+def save_to_cache(string, data)
 	file = open(File.join(CachePath, md5(string)), "w")
 	file.write(data)
 	file.close()
 end
 
-def loadfromcache(string)
+def load_from_cache(string)
 	file = File.join(CachePath, md5(string))
 	return open(file).read().strip() if File.exists?(file)
 	return nil
